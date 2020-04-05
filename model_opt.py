020a54948f1224f515d86523f790fe59d5596613
Naoki Nakamichi <ronnao0516@gmail.com>
2020-04-05 14:41:05 +0900

    complete SGD notebook

--- model_opt.py
+++ model_opt.py
@@ -44,10 +44,10 @@ class Perm(models.Model):
     def f_opt(self,w):
         w = np.array(w)
         d = w.shape[0]
-        minmun = 1
-        self.w_star = []
-        for i in range(d):
-            self.w_star.append((minmun / (i + 1)))
+        minimum = 1
+        self.w_star = [1]
+        for i in range(d-1):
+            self.w_star.append((minimum / 2))
         self.w_star = np.array(self.w_star)
 
         tmp = 0
@@ -63,10 +63,10 @@ class Perm(models.Model):
     def g_opt(self,w):
         w = np.array(w)
         d = w.shape[0]
-        minmun = 1
-        self.w_star = []
-        for i in range(d):
-            self.w_star.append((minmun / (i + 1)))
+        minimum = 1
+        self.w_star = [1]
+        for i in range(d-1):
+            self.w_star.append((minimum / 2))
         self.w_star = np.array(self.w_star)
         tmp = np.zeros(w.shape)
         for i in list(range(d)):
@@ -208,10 +208,17 @@ class Trid(models.Model):
     def __init__(self, name="trid",err=0.0):
         super(Trid, self).__init__(name=name)
         self.err = err
+        self.w_star = [2,2]
 
     def f_opt(self,w):
         w = np.array(w)
         d = w.shape[0]
+        self.w_star = []
+        for i in range(d):
+            minimum = (i + 1) * (d + 1 - (i + 1))
+            self.w_star.append(minimum)
+        self.w_star = np.array(self.w_star)
+
         tmp_1 = np.zeros(w.shape)
         tmp_2 = np.zeros(w.shape)
         for i in range(d):
@@ -220,11 +227,19 @@ class Trid(models.Model):
             tmp_2 += w[i] * w[i-1]
         tmp = tmp_1 - tmp_2
 
+        if self.err != 0:
+            tmp = tmp + self.err * np.random.randn(1)
+
         return tmp[0]
 
     def g_opt(self,w):
         w = np.array(w)
         d = w.shape[0]
+        self.w_star = []
+        for i in range(d):
+            minimum = (i + 1) * (d + 1 - (i + 1))
+            self.w_star.append(minimum)
+        self.w_star = np.array(self.w_star)
         tmp = np.zeros(w.shape)
         for i in range(d):
             if i == 0:
@@ -234,56 +249,87 @@ class Trid(models.Model):
             else:
                 tmp[i] = 2 * (w[i] - 1) - (w[i-1] + w[i+1])
 
+        if self.err != 0:
+            tmp = tmp + self.err * np.random.randn(1)
+
         return tmp
 
 class ThreeHumpCamel(models.Model):
     def __init__(self, name="Three-Hump Camel",err=0.0):
         super(ThreeHumpCamel, self).__init__(name=name)
         self.err = err
+        self.w_star = np.array([0,0])
 
     def f_opt(self,w):
         w1,w2 = w[0],w[1]
-        return 2 * w1 ** 2 - 1.05 * w1 ** 4 + (w1 ** 6) / 6 + w1 * w2 + w2 ** 2
+        tmp = 2 * w1 ** 2 - 1.05 * w1 ** 4 + (w1 ** 6) / 6 + w1 * w2 + w2 ** 2
+        if self.err != 0:
+            tmp = tmp + self.err * np.random.randn(1)
+        return tmp
 
     def g_opt(self,w):
         w1,w2 = w[0],w[1]
         tmp_1 = 4 * w1 - 4.2 * w1 ** 3 + w1 ** 5 + w2
         tmp_2 = w1 + 2 * w2
-        return [tmp_1,tmp_2]
+        tmp = np.array([tmp_1,tmp_2])
+        if self.err != 0:
+            tmp = tmp + self.err * np.random.randn(2)
+        return tmp
 
 class SixHumpCamel(models.Model):
     def __init__(self, name="Six-Hump Camel",err=0.0):
         super(SixHumpCamel, self).__init__(name=name)
         self.err = err
+        self.w_star = np.array([0.0898,-0.7126])
 
     def f_opt(self,w):
         w1,w2 = w[0],w[1]
-        return (4 - 2.1 * (w1 ** 2) + (w1 ** 4) / 3 ) * (w1 ** 2) + w1 * w2 + (-4 + 4 * (w2 ** 2)) * (w2 ** 2)
+        tmp = (4 - 2.1 * (w1 ** 2) + (w1 ** 4) / 3 ) * (w1 ** 2) + w1 * w2 + (-4 + 4 * (w2 ** 2)) * (w2 ** 2)
+        if self.err != 0:
+            tmp = tmp + self.err * np.random.randn(1)
+        return tmp
 
     def g_opt(self,w):
         w1,w2 = w[0],w[1]
         tmp_1 = 8 * w1 - 8.4 * (w1 ** 3) + 2 * (w1 ** 5) + w2
         tmp_2 = w1 - 8 * w2 + 16 * (w2 ** 3)
-        return [tmp_1,tmp_2]
+        tmp = np.array([tmp_1,tmp_2])
+        if self.err != 0:
+            tmp = tmp + self.err * np.random.randn(2)
+        return tmp
 
 class DixonPrice(models.Model):
     def __init__(self, name="Dixon-Price",err=0.0):
         super(DixonPrice, self).__init__(name=name)
         self.err = err
+        self.w_star = np.array([0, 2 ** (-1/2)])
 
     def f_opt(self,w):
         w = np.array(w)
         d = w.shape[0]
+        self.w_star = []
+        for i in range(1,d+1):
+            minimum = 2 ** ((-1) * ((2 ** i - 2) / 2 ** i))
+            self.w_star.append(minimum)
+        self.w_star = np.array(self.w_star)
         tmp = np.zeros(w.shape)
         for i in range(1,d):
             tmp += (i+1) * (2 * w[i] ** 2  - w[i-1]) ** 2
         tmp += (w[0]-1) ** 2
+
+        if self.err != 0:
+            tmp = tmp + self.err * np.random.randn(1)
 
         return tmp[0]
 
     def g_opt(self,w):
         w = np.array(w)
         d = w.shape[0]
+        self.w_star = []
+        for i in range(1,d+1):
+            minimum = 2 ** ((-1) * ((2 ** i - 2) / 2 ** i))
+            self.w_star.append(minimum)
+        self.w_star = np.array(self.w_star)
         tmp = np.zeros(w.shape)
         for i in range(d):
             if i == 0:
@@ -293,6 +339,9 @@ class DixonPrice(models.Model):
             else:
                 tmp[i] =  8 * w[i] * d * (i + 1) * (2 * w[i] ** 2 - w[i-1]) - (i + 2) * 2 * (2 * w[i+1] ** 2 - w[i])
 
+        if self.err != 0:
+            tmp = tmp + self.err * np.random.randn(1)
+
         return tmp
 
 
@@ -300,20 +349,27 @@ class RosenBrock(models.Model):
     def __init__(self, name="RosenBrock",err=0.0):
         super(RosenBrock, self).__init__(name=name)
         self.err = err
+        self.w_star = np.ones(2)
 
     def f_opt(self,w):
         w = np.array(w)
         d = w.shape[0]
+        self.w_star = np.ones(d)
         tmp = np.zeros(w.shape)
         for i in range(0, d - 1):
             tmp_1 = 100 * (w[i+1] - w[i] ** 2) ** 2
             tmp_2 = (w[i] -1) ** 2
             tmp += tmp_1 + tmp_2
+
+        if self.err != 0:
+            tmp = tmp + self.err * np.random.randn(1)
+
         return tmp[0]
 
     def g_opt(self,w):
         w = np.array(w)
         d = w.shape[0]
+        self.w_star = np.ones(d)
         tmp = np.zeros(w.shape)
         for i in range(d):
             if i == 0:
@@ -322,5 +378,8 @@ class RosenBrock(models.Model):
                 tmp[i] = 100 * 2 * (w[i] - w[i-1] ** 2)
             else:
                 tmp[i] = 100 * (-4) * w[i] * (w[i+1] - w[i] ** 2) + 2 * (w[i] - 1) + 100 * 2 * (w[i] - w[i-1] ** 2)
+
+        if self.err != 0:
+            tmp = tmp + self.err * np.random.randn(1)
 
         return tmp
