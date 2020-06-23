8fe741d4940b6d3423ed31b474bffae29f6f6a70
Naoki Nakamichi <ronnao0516@gmail.com>
2020-06-24 03:08:40 +0900

    tmp

--- GD/helper.py
+++ GD/helper.py
@@ -1,16 +1,26 @@
 import numpy as np
 import random
 
-def gauss(w):
+def gauss(w,loc=0, scale=1):
     np.random.seed()
-    return np.random.randn(*w.shape)
+    return np.random.normal(loc=loc,scale=scale,size=w.shape)
 
-def lognormal(w):
+def lognormal(w,mean=0,sigma=1):
     np.random.seed()
-    return np.random.lognormal(*w.shape)
+    return np.random.lognormal(mean=mean,sigma=sigma,size=w.shape)
 
 def pareto(w,a=1):
     np.random.seed()
     return np.random.pareto(*w.shape,a)
+
+def iqr(x):
+    q75, q25 = np.percentile(x, [75 ,25])
+    iqr = q75 - q25
+    return iqr
+
+def signal_noise():
+    pass
+
+def g_norm()
     
     
