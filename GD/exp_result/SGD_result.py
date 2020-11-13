241fddbbd964b15d1c424941337fb74b6fd4fea4
Naoki Nakamichi <ronnao0516@gmail.com>
2020-11-13 15:21:41 +0900

    tmp

--- GD/exp_result/SGD_result.py
+++ GD/exp_result/SGD_result.py
@@ -31,25 +31,47 @@ noise_value = np.zeros(2)
 var = 50 # iqr 70~80
 batchsize = 100
 _t_max = 100
-noise_data = noise.Gauss(mean=0, sigma=var, dim=2, n=batchsize * _t_max).generate()
-lr_list = [0.00001, 0.00005, 0.0001, 0.005, 0.001, 0.01, 0.1, 1]
+lr_list = [0.0000000001, 0.00005, 0.0001, 0.005, 0.001, 0.01, 0.1, 1]
 model_list = [model_opt.Perm, model_opt.RosenBrock, model_opt.SumOfDifferent, model_opt.SumSquares]
 noise_var_list = [0, 10, 30, 50, 70, 90, 100]
 w_range = [-5,5]
-for model in model_list:
+dim = 100
+lr_frag = []
+noise_var_flag = []
+f_value = []
+model_name = []
+for model in tqdm(model_list):
     for var in noise_var_list:
         for lr in lr_list:
-            for _ in range(100):
+            for _ in range(1):
 
-                w_init = (w_range[1] - w_range[0]) * np.random.rand(10000) + w_range[0]
+                w_init = (w_range[1] - w_range[0]) * np.random.rand(dim) + w_range[0]
                 algo = SGD(w_init=w_init, t_max=_t_max, a=lr)
+                name = model().name
                 try:
                     for _ in algo:
-                        noise_value = noise.Gauss(mean=0, sigma=var, dim=10000, n=1).generate()
+                        noise_value = noise.Gauss(mean=0, sigma=var, dim=dim, n=1).generate()[0]
                         f = model(noise_value=noise_value)
                         algo.update(model=f)
+                    lr_frag.append(lr)
+                    noise_var_flag.append(var)
+                    model_name.append(name)
+                    try:
+                        f_value.append(f.f_opt(algo.w))
+                    except RuntimeWarning:
+                        f_value.append(-1)
                 except RuntimeWarning:
-                    print("fuga")
+                    lr_frag.append(lr)
+                    noise_var_flag.append(var)
+                    model_name.append(name)
+                    f_value.append(-1)
+
+
+df = pd.DataFrame([model_name,lr_frag,noise_var_flag,f_value]).T
+dt_now = datetime.datetime.now()
+df.to_csv('GD/exp_result/SGD_result/SGD_result{}.csv'.format(dt_now))
+
+
 
 
 
