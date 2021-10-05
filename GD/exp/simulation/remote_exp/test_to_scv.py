94c934933a9d863f267bfd8c9583fd5465c9db22
Naoki Nakamichi <ronnao0516@gmail.com>
2021-10-05 11:13:59 +0900

    hoge

--- GD/exp/simulation/remote_exp/test_to_scv.py
+++ GD/exp/simulation/remote_exp/test_to_scv.py
@@ -4,12 +4,10 @@ import pandas as pd
 import datetime
 import os, sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-
 if __name__ == "__main__":
     args = sys.argv
 
     now = datetime.datetime.now()
     hoge = np.arange(9).reshape(3,3)
     df = pd.DataFrame(hoge, columns=[1, 2, 3])
-    df.to_csv("GD/exp/simulation/remote_exp/remote_save_result", index=False)
+    df.to_csv("remote_save_result/test.csv", index=False)
