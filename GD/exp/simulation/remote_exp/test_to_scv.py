234777e8d3c226dc7bb93f864b601cc2f68726a0
Naoki Nakamichi <ronnao0516@gmail.com>
2021-10-05 11:04:01 +0900

    hoge

--- GD/exp/simulation/remote_exp/test_to_scv.py
+++ GD/exp/simulation/remote_exp/test_to_scv.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     args = sys.argv
 
     now = datetime.datetime.now()
-    df = pd.DataFrame(np.array([4, 5, 6]).T, columns=[1, 2, 3])
+    hoge = np.arange(9).reshape(3,3)
+    df = pd.DataFrame(hoge, columns=[1, 2, 3])
     df.to_csv("remote_save_result/test.csv", index=False)
