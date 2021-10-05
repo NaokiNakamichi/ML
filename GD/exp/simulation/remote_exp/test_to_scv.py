a931e2b935b96be8b2fd2d73cca2cf987aa7dee1
Naoki Nakamichi <ronnao0516@gmail.com>
2021-10-05 10:14:33 +0900

    hoge

--- GD/exp/simulation/remote_exp/test_to_scv.py
+++ GD/exp/simulation/remote_exp/test_to_scv.py
@@ -15,5 +15,6 @@ if __name__ == "__main__":
     now = datetime.datetime.now()
     df = pd.DataFrame(np.array([4, 5, 6]), columns=[1, 2, 3])
     df.to_csv(
-        f"remote_save_result/{now:%m月%d日%H:%M:%S}test.csv",
-        index=False)
+        f"remote_save_result/{now:%m月%d日%H:%M:%S}test.csv",index=False)
+
+
