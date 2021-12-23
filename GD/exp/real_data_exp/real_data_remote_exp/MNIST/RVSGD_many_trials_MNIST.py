af7a821f08e65546feb6b9289d9c0ba0b317395b
Naoki Nakamichi <ronnao0516@gmail.com>
2021-12-23 11:44:42 +0900

    hoge

---
+++ GD/exp/real_data_exp/real_data_remote_exp/MNIST/RVSGD_many_trials_MNIST.py
@@ -0,0 +1,64 @@
+from ML2_lib import format_data
+from ML2_lib import RV_SGD_Torch
+
+from sklearn.model_selection import train_test_split
+
+import joblib
+
+import datetime
+
+from tqdm import tqdm
+
+import pandas as pd
+import numpy as np
+
+if __name__ == "__main__":
+    trial_num = 3
+
+    X_train, y_train, X_test, y_test = format_data.MNIST_data().return_data()
+    X_train, X_valid = train_test_split(X_train, test_size=0.1, shuffle=False)
+    y_train, y_valid = train_test_split(y_train, test_size=0.1, shuffle=False)
+
+    lr_list = [0.1,0.01,0.001]
+    model_type_list = ["linear", "FF_L1", "FF_L2"]
+
+    result_loss = []
+    result_accuracy = []
+
+    col_name_list = []
+
+    for lr, model_type in zip(lr_list,model_type_list):
+        for k in range(1, 10):
+            col_name_list.append(f"RVSGD_model_{model_type}_lr_{lr}_k_{k}")
+
+
+    for i in tqdm(range(trial_num)):
+
+        trial_result_loss = []
+        trial_result_accuracy = []
+
+        for lr, model_type in zip(lr_list,model_type_list):
+
+            hoge = RV_SGD_Torch.RVSGDByTorch(lr=0.01)
+            k_model_list = []
+
+            for k in range(1, 10):
+                result_model = hoge.run_RVSGD(x_train=X_train, y_train=y_train, valid_x=X_valid, valid_y=y_valid, k=k,
+                                              model_type=model_type)
+                k_model_list.append(result_model)
+
+            for i_model in k_model_list:
+                test_loss, accuracy = hoge.prediction(x=X_test, y=y_test, model=i_model, is_print=False)
+                trial_result_accuracy.append(accuracy)
+                trial_result_loss.append(test_loss)
+
+
+        result_loss.append(trial_result_loss)
+        result_accuracy.append(trial_result_accuracy)
+
+    now = datetime.datetime.now()
+
+    result_accuracy_pd = pd.DataFrame(np.array(result_accuracy).T, columns=col_name_list)
+
+    result_accuracy_pd.to_csv(f"result_data/RVSGD_MNIST_trial_{trial_num}_{now}.csv",index=False)
+
